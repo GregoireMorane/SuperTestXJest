07717c5622a764b3720ddb75aeaa89d0aea9e4dc
GrégoireMorane <gh.morane@gmail.com>
2019-08-20 09:44:45 +0200

    feat(test): setup test

---
+++ app.integration.spec.js
@@ -0,0 +1,44 @@
+const request = require("supertest");
+
+const app = require("./app");
+const agent = request.agent(app);
+
+describe("app", () => {
+  describe("when authenticated", () => {
+    beforeEach(async () => {
+      await agent
+        .post("/login")
+        .send("username=randombrandon&password=randompassword");
+    });
+
+    describe("POST /messages", () => {
+      describe("with non-empty content", () => {
+        describe("with JavaScript code in personalWebsiteURL", () => {
+          it("responds with error", async done => {
+            agent
+              .post("/messages")
+              .send(
+                "content=ttt&personalWebsiteURL=javascript:alert('Hacked!');"
+              )
+              .then(response => {
+                expect(response.statusCode).toBe(400);
+                done();
+              });
+          });
+        });
+
+        describe("with HTTP URL in personalWebsiteURL", () => {
+          it("responds with success", async done => {
+            agent
+              .post("/messages")
+              .send("content=ttt&personalWebsiteURL=https://www.lemonde.fr")
+              .then(response => {
+                expect(response.statusCode).toBe(201);
+                done();
+              });
+          });
+        });
+      });
+    });
+  });
+});
